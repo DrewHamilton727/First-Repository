d33fb9a50ff953ecbc8a2b1bbb2345528a4609ce
Drew Hamilton <DrewRHamilton@gmail.com>
2015-01-22 15:22:33 -0500

    Assignment 8 initial submission

---
+++ JS8/Assignment 8.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var teams =["Buccaneers", "Falcons", "Saints", "Panthers"]
+
+for (teams=0; teams<4; teams++){
+	console.log(1)
+}
+
+for (i=0; i<10; i++){
+	console.log(i)
+}
+
+for (teams=0; teams<4; teams++){
+	console.log(teams.length)
+
+}
+for (teams=0; teams<4; teams++){
+	console.log("p")
+}
+
+var car = {doors:4, honk:"honk"}
+	console.log(car)
+
+
