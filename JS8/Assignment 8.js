8435a4a490c2b7252906dc784760700a8d9d47db
Drew Hamilton <DrewRHamilton@gmail.com>
2015-01-29 16:13:52 -0500

    Resume Site

--- JS8/Assignment 8.js
+++ JS8/Assignment 8.js
@@ -1,24 +1,26 @@
 "use strict";
 
-var teams =["Buccaneers", "Falcons", "Saints", "Panthers"]
+var k = ["Buccaneers", "Falcons", "Saints", "Panthers"];
+console.log(k[1]);
 
-for (teams=0; teams<4; teams++){
-	console.log(1)
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+};
+
+for (var i = 0; i < k.length; i++) {
+    console.log(k[i]);
+
+};
+
+for (var i = 0; i < k.length; i++) {
+    if (k[i].substring(0, 1) === "P") {
+        console.log(k[i]);
+    };
+};
+
+
+var car = {
+    doors: 4,
+    honk: "honk"
 }
-
-for (i=0; i<10; i++){
-	console.log(i)
-}
-
-for (teams=0; teams<4; teams++){
-	console.log(teams.length)
-
-}
-for (teams=0; teams<4; teams++){
-	console.log("p")
-}
-
-var car = {doors:4, honk:"honk"}
-	console.log(car)
-
-
+console.log(car);
