64256155caaaa9fecbde3f16f5e3cffec91776b2
Drew Hamilton <DrewRHamilton@gmail.com>
2015-02-16 12:42:46 -0500

    initial portfolio push

---
+++ portfolio/javascript/portfolio.js
@@ -0,0 +1,32 @@
+"use strict";
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : false, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+      autoPlay: true,
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+      $("#owl-demo2").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+ 
+});
